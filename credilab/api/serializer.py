793fa63fc9952a9873f5aef139315019af1957d0
marcos <marc0sdurval20@gmail.com>
2022-09-07 11:39:32 -0300

    feat: add business rules

--- credilab/api/serializer.py
+++ credilab/api/serializer.py
@@ -2,8 +2,26 @@ from rest_framework import serializers
 from credilab.models import Client
 
 
-class ClientSeriliazer(serializers.ModelSerializer):
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = "__all__"
+
+    def validate(self, data):
+        latitude = data["latitude"]
+        longitude = data["longitude"]
+
+        if not ((-90 <= latitude <= 90) or (-180 <= longitude <= 180)):
+            raise serializers.ValidationError("Invalid latitude or longitude")
+        return data
+
+
+class ClientSerializerEventString(serializers.ModelSerializer):
+    event = serializers.SerializerMethodField()
 
     class Meta:
         model = Client
         fields = "__all__"
+
+    def get_event(self, obj):
+        return obj.get_event_display()
--- credilab/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from credilab.api.serializer import ClientSeriliazer
-
-from credilab.models import Client
-
-
-class ClientViewSet(viewsets.ModelViewSet):
-    queryset = Client.objects.all()
-    serializer_class = ClientSeriliazer
